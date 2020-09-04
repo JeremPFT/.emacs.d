8ba901416e428e0e0161c6c1891d03d1322d7ccc
jpiffret <jpiffret@ingenico.com>
2020-09-04 18:24:04 +0200

    Updates.

--- emacs-custom.el
+++ emacs-custom.el
@@ -17,15 +17,19 @@
  '(org-id-link-to-org-use-id (quote create-if-interactive-and-no-custom-id))
  '(safe-local-variable-values
    (quote
-    ((eval add-hook
-	   (quote after-save-hook)
-	   (lambda nil
-	     (org-babel-tangle)))))))
+    ((eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-model-service")
+     (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-repository")
+     (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-model-object")
+     (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/run")
+     (eval progn
+	   (org-babel-tangle)
+	   (when
+	       (y-or-n-p "load init.el? ")
+	     (load
+	      (concat user-emacs-directory "init.el"))))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#050000" :foreground "#bbe0f0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "outline" :family "Courier New"))))
- )
-
+ '(default ((t (:inherit nil :stipple nil :background "#050000" :foreground "#bbe0f0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "outline" :family "Courier New")))))
