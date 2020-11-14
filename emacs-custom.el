37d3695ce6c9a130758cfc9aa4e5bdfd7b4e4a35
Jeremy <jeremy@piffret.fr>
2020-11-14 15:10:26 +0100

    Extract setup from emacs-custom.el.

--- emacs-custom.el
+++ emacs-custom.el
@@ -3,22 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(column-number-mode t)
- '(desktop-path '("~/.emacs.d/sessions"))
- '(enable-remote-dir-locals t)
- '(grep-find-command
-   '("find . -type f -exec grep --color=always -nH -e  \"{}\" +" . 49))
- '(indent-tabs-mode nil)
  '(org-agenda-files
-   (quote
-    ("~/workspace/org/bookmarks/z/bookmarks-z-session.org.txt" "c:/Users/jeremy/AppData/Roaming/workspace/org/agenda/agenda.org" "c:/Users/jeremy/AppData/Roaming/workspace/org/agenda/dates.org" "c:/Users/jeremy/AppData/Roaming/workspace/org/agenda/dette_cetelem.org" "c:/Users/jeremy/AppData/Roaming/workspace/org/agenda/pauses.org" "~/workspace/org/reference-cards/emacs-reference-card.org" "~/.emacs.d/lisp/yasnippet/org-snippet-new-link.org" "~/.emacs.d/README.org")))
+   '("~/workspace/org/bookmarks/z/bookmarks-z-session.org.txt" "c:/Users/jeremy/AppData/Roaming/workspace/org/agenda/dates.org" "c:/Users/jeremy/AppData/Roaming/workspace/org/agenda/pauses.org" "~/workspace/org/reference-cards/emacs-reference-card.org" "~/.emacs.d/lisp/yasnippet/org-snippet-new-link.org" "~/.emacs.d/README.org"))
  '(safe-local-variable-values
-   (quote
-    ((eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/utils-test")
+   '((eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/utils-test")
      (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-repository-test")
      (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-model-service-test")
      (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-model-object-test")
@@ -29,8 +17,6 @@
      (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-repository")
      (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-model-object")
      (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/run"))))
- '(show-paren-mode t)
- '(tramp-connection-properties '(("/plinkx" "remote-shell" "/bin/bash"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
