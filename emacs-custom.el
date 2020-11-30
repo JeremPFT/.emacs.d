98679c59b02a468c5a81b31de7cb21d8b66d5627
Jeremy Piffret <j.piffret@gmail.com>
2020-11-30 17:25:43 +0100

    Add cnd-161.el local eval to safe-local-variable-values.

--- emacs-custom.el
+++ emacs-custom.el
@@ -4,9 +4,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/workspace/org/bookmarks/z/bookmarks-z-session.org.txt" "c:/Users/jeremy/AppData/Roaming/workspace/org/agenda/dates.org" "c:/Users/jeremy/AppData/Roaming/workspace/org/agenda/pauses.org" "~/workspace/org/reference-cards/emacs-reference-card.org" "~/.emacs.d/lisp/yasnippet/org-snippet-new-link.org" "~/.emacs.d/README.org"))
+   (quote
+    ("~/workspace/org/bookmarks/z/bookmarks-z-session.org.txt" "c:/Users/jeremy/AppData/Roaming/workspace/org/agenda/dates.org" "c:/Users/jeremy/AppData/Roaming/workspace/org/agenda/pauses.org" "~/workspace/org/reference-cards/emacs-reference-card.org" "~/.emacs.d/lisp/yasnippet/org-snippet-new-link.org" "~/.emacs.d/README.org")))
  '(safe-local-variable-values
-   '((eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/utils-test")
+   (quote
+    ((eval let
+           ((elisp-file "output/cnd-161.el"))
+           (make-directory "output" t)
+           (unless
+               (file-exists-p elisp-file)
+             (org-babel-tangle))
+           (load-file elisp-file))
+     (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/utils-test")
      (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-repository-test")
      (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-model-service-test")
      (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-model-object-test")
@@ -16,7 +25,7 @@
      (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-model-service")
      (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-repository")
      (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/ata-model-object")
-     (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/run"))))
+     (eval load "~/workspace/ada_test_architectures/src/.emacs_prj_settings/run")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
