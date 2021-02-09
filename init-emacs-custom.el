b4e57d1f2616a15e53b194dc8e7e98c99798b283
Jeremy <jeremy@piffret.fr>
2021-02-10 00:08:54 +0100

    WIP: emacs configuration.

--- init-emacs-custom.el
+++ init-emacs-custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(feather leaf-keywords blackout el-get hydra leaf)))
+ '(package-selected-packages
+   '(flx zenburn-theme poporg org-web-tools org-mind-map org-generate org-brain ob-async magit-gitflow link-hint leaf-tree leaf-keywords hydra htmlize git-link f el-get counsel blackout avy-menu)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
